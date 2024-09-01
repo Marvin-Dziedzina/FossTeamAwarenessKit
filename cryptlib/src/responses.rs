0e3b6c883a13b6bcfa86756f22b3f59dd0b98e0b
Mixaffected <marvin.dziedzina@gmail.com>
2024-09-02 00:23:16 +0200

    Added derive serde and debug

--- cryptlib/src/responses.rs
+++ cryptlib/src/responses.rs
@@ -1,6 +1,9 @@
+use serde::{Deserialize, Serialize};
+
 use crate::{aes::AesCiphertext, rsa::RsaCiphertext};
 
 /// `rsa_ciphertext` holds the encrypted aes key. `aes_ciphertext` holds the aes encrypted data.
+#[derive(Debug, Serialize, Deserialize)]
 pub struct CiphertextData {
     rsa_ciphertext: RsaCiphertext,
     aes_ciphertext: AesCiphertext,
